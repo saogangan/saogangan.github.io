46d16a17838b5f12350006840c7dfa2d6315834f
saogangan <56327303+saogangan@users.noreply.github.com>
2019-10-09 11:30:09 +0900

    changes

--- js/index.20191009110526.js
+++ js/index.20191009110526.js
@@ -5,6 +5,10 @@ $.fn.hasAttr = function(name) { var attr = $(this).attr(name); return typeof att
 $(document).ready(function() {
 r=function(){dpi=window.devicePixelRatio;$('.js').attr('src', (dpi>1) ? 'images/e387ece8-9da4-4863-9866-b750963a2a34-400.jpeg' : 'images/e387ece8-9da4-4863-9866-b750963a2a34-200.jpeg');};
 if(!window.HTMLPictureElement){r();}
+if(! document.implementation.hasFeature("http://www.w3.org/TR/SVG11/feature#Image", "1.1")) {
+$('.js-2').attr('src', 'images/fb-30.png');
+
+}
 (function(){$('a[href^="#"]').each(function(){$(this).click(function(){var t=this.hash.length>1?$('[name="'+this.hash.slice(1)+'"]').offset().top:0;return $("html, body").animate({scrollTop:t},400),!1})})})();
 initMenu($('#menu-1')[0]);
 
