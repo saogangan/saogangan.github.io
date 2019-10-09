c3ef470606e9bb335ed4a4845f637193c2cf69b9
saogangan <56327303+saogangan@users.noreply.github.com>
2019-10-09 11:44:06 +0900

    3

--- js/index.20191009114358.js
+++ js/index.20191009114358.js
@@ -3,14 +3,18 @@ $.fn.hasAttr = function(name) { var attr = $(this).attr(name); return typeof att
 
 
 $(document).ready(function() {
-r=function(){dpi=window.devicePixelRatio;$('.js').attr('src', (dpi>1) ? 'images/e387ece8-9da4-4863-9866-b750963a2a34-400.jpeg' : 'images/e387ece8-9da4-4863-9866-b750963a2a34-200.jpeg');
-$('.js-2').attr('src', (dpi>1) ? 'images/296002df-b754-4a5d-8cbf-2d81782959d1-736.jpeg' : 'images/296002df-b754-4a5d-8cbf-2d81782959d1-368.jpeg');};
+r=function(){dpi=window.devicePixelRatio;$('.js').attr('src', (dpi>1) ? 'images/e387ece8-9da4-4863-9866-b750963a2a34-540.jpeg' : 'images/e387ece8-9da4-4863-9866-b750963a2a34-270.jpeg');
+$('.js-3').attr('src', (dpi>1) ? 'images/296002df-b754-4a5d-8cbf-2d81782959d1-736.jpeg' : 'images/296002df-b754-4a5d-8cbf-2d81782959d1-368.jpeg');};
 if(!window.HTMLPictureElement){r();}
 if(! document.implementation.hasFeature("http://www.w3.org/TR/SVG11/feature#Image", "1.1")) {
-$('.js-3').attr('src', 'images/fb-30.png');
+$('.js-4').attr('src', 'images/fb-30.png');
 
 }
 (function(){$('a[href^="#"]').each(function(){$(this).click(function(){var t=this.hash.length>1?$('[name="'+this.hash.slice(1)+'"]').offset().top:0;return $("html, body").animate({scrollTop:t},400),!1})})})();
 initMenu($('#menu-1')[0]);
+var wl = new woolite();
+wl.init();
+wl.addAnimation($('.js-2')[0], "1.00s", "0.00s", 1, 100);
+wl.start();
 
 });
